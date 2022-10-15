14b3a8f70dd4dc12940455e071897f03a8e297d1
daleyu <yudale2003@gmail.com>
2022-10-15 19:30:23 -0400

    Side Bar Changes on proxy Name

--- pages/proxy/[step].tsx
+++ pages/proxy/[step].tsx
@@ -1,10 +1,17 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { ReactNode, useState } from "react";
 import { ProxyNameForm } from "../../components/proxyNameForm";
 import { ProxyRecipientsForm } from "../../components/proxyRecipientsForm";
+import {
+  ImageSideContainer,
+  SideContainer,
+  TextSideContainer,
+} from "../../layouts/proxy.style.ts";
 import { SplitLayout } from "../../layouts/splitLayout";
 import { fetchApi } from "../../lib/fetchApi";
+import HAND from "../../public/images/hand.png";
 import { ProxyFormStore } from "../../store/proxyFormStore";
 import { ComposerFrontendModel } from "../../types/frontendModels";
 
@@ -88,7 +95,27 @@ const Proxy: NextPage = () => {
     </>
   );
 
-  return <SplitLayout sidebar={<p>sidebar</p>} main={main} />;
+  return (
+    <SplitLayout
+      sidebar={
+        <div>
+          <SideContainer>
+            <ImageSideContainer>
+              <Image src={HAND} layout="responsive" alt="tsah" />
+            </ImageSideContainer>
+            <TextSideContainer>
+              <p>
+                <b> Let's get started on creating your memory capsules!</b>
+                <br></br>When would you like to retrieve your memory capsules
+                and remember you? When do you to retrieve it?
+              </p>
+            </TextSideContainer>
+          </SideContainer>
+        </div>
+      }
+      main={main}
+    />
+  );
 };
 
 export default Proxy;
