283993111ae4fe3c93ae41b8543512115e69b1f0
Alex Chen <axchen7@outlook.com>
2022-10-15 12:42:26 -0400

    Add proxyFormStore

---
+++ store/proxyFormStore.ts
@@ -0,0 +1,49 @@
+import { computed, makeAutoObservable } from "mobx";
+import { ComposerRequestBody } from "../types/requestBodies";
+
+export class ProxyFormStore {
+  composerName = "";
+  proxyName = "";
+  recipients: Recipient[] = [];
+
+  constructor() {
+    makeAutoObservable(this, {
+      recipients: computed.struct,
+    });
+  }
+
+  get composerRequestBody(): ComposerRequestBody {
+    return {
+      name: this.composerName,
+      proxyName: this.proxyName,
+      recipients: this.recipients.map((recipient) => ({
+        name: recipient.name,
+        occasions: recipient.occasions.map((occasion) => ({
+          label: occasion.label,
+          date: occasion.date.toISOString(),
+        })),
+      })),
+    };
+  }
+}
+
+export class Recipient {
+  name = "";
+  occasions: Occasion[] = [];
+
+  constructor() {
+    makeAutoObservable(this, {
+      occasions: computed.struct,
+    });
+  }
+}
+
+export class Occasion {
+  label = "";
+  date: Date;
+
+  constructor(defaultDate: Date) {
+    this.date = defaultDate;
+    makeAutoObservable(this);
+  }
+}
