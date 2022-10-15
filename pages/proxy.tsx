4e67aaaf0783e900a7b17bb9234d9a9c5bcb1687
daleyu <yudale2003@gmail.com>
2022-10-15 03:08:48 -0400

    aoeu

--- pages/proxy.tsx
+++ pages/proxy.tsx
@@ -1,5 +1,10 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 import styles from "../styles/proxy.css";
+import logo from "../images/logo.png";
+import NAME from "../images/name-button.png";
+import COMPOSE from "../images/compose-button.png";
 
 const Home: NextPage = () => {
   return (
