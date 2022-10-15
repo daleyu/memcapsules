ef7e45f90d85bf5f53574a9d239af17d1d7fa277
daleyu <yudale2003@gmail.com>
2022-10-15 03:59:57 -0400

    work

--- pages/proxy.tsx
+++ pages/proxy.tsx
@@ -19,6 +19,38 @@ const Home: NextPage = () => {
         <span className={styles.logo}>
           <Image src={logo} alt="Logo" />
         </span>
+        <span className={styles.name}>
+          <Image src={NAME} alt="name" />
+        </span>
+        <span className={styles.compose}>
+          <Image src={COMPOSE} alt="compose" />
+        </span>
+        <form action="/action_page.php">
+          <h1>Login Form</h1>
+          <div className={styles.formcontainer}>
+            <hr />
+            <div className={styles.container}>
+              <label for="uname">
+                <strong>Name</strong>
+              </label>
+              <input type="text" placeholder="Enter Name" name="uname" required>
+                {" "}
+              </input>
+              <label for="psw">
+                <strong>Composer</strong>
+              </label>
+              <input
+                type="password"
+                placeholder="Enter Composer"
+                name="psw"
+                required
+              >
+                {" "}
+              </input>
+            </div>
+            <button type="submit">Commit</button>
+          </div>
+        </form>
       </main>
     </div>
   );
