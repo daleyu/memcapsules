4ccd936695dfa9cce6a9da4d69bda24141e4471f
daleyu <yudale2003@gmail.com>
2022-10-15 02:09:39 -0400

    created tsx files

---
+++ pages/proxy.tsx
@@ -0,0 +1,46 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    
+    <div class="col-md-6 wow animated fadeInRight" data-wow-delay=".2s">
+        <form class="shake" role="form" method="post" id="contactForm" name="contact-form" data-toggle="validator">
+            <!-- Name -->
+            <div class="form-group label-floating">
+                <label class="control-label" for="name">Name</label>
+                <input class="form-control" id="name" type="text" name="name" required data-error="Please enter your name">
+                <div class="help-block with-errors"></div>
+            </div>
+            <!-- email -->
+            <div class="form-group label-floating">
+                <label class="control-label" for="email">Email</label>
+                <input class="form-control" id="email" type="email" name="email" required data-error="Please enter your Email">
+                <div class="help-block with-errors"></div>
+            </div>
+            <!-- Subject -->
+            <div class="form-group label-floating">
+                <label class="control-label">Phone #</label>
+                <input class="form-control" id="msg_subject" type="tel" name="Phone" required data-error="Please enter your message subject">
+                <div class="help-block with-errors"></div>
+            </div>
+            <!-- Message -->
+            <div class="form-group label-floating">
+                <label for="message" class="control-label">Message</label>
+                <textarea class="form-control" rows="3" id="message" name="message" required data-error="Write your message"></textarea>
+                <div class="help-block with-errors"></div>
+            </div>
+            <!-- Form Submit -->
+            <div class="form-submit mt-5">
+                <button class="btn btn-primary" type="submit" id="form-submit"><i class="material-icons mdi mdi-message-outline"></i> Send Message</button>
+                <div id="msgSubmit" class="h3 text-center hidden"></div>
+                <div class="clearfix"></div>
+            </div>
+        </form>
+    </div>
+  );
+};
+
+export default Home;
