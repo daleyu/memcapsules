8f1f3802f148369646602e2ebe0d69be1f90d291
alexkoletsos <97374144+alexkoletsos@users.noreply.github.com>
2022-10-15 14:59:49 -0400

    j

--- pages/proxy/[step].tsx
+++ pages/proxy/[step].tsx
@@ -2,13 +2,9 @@ import type { NextPage } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { ReactNode, useCallback, useState } from "react";
-<<<<<<< HEAD
 import { ProxyNameForm } from "../../components/ProxyNameForm";
 import { ReceipientNameForm } from "../../components/receipientNameForm";
 import { SideContainer } from "../../layouts/proxy.style.ts";
-=======
-import { ProxyNameForm } from "../../components/proxyNameForm";
->>>>>>> 3ba29941a9550b10fed852232144cc6153a05b36
 import { SplitLayout } from "../../layouts/splitLayout";
 import { fetchApi } from "../../lib/fetchApi";
 import HAND from "../../public/images/hand.png";
