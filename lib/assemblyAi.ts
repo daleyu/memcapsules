7510fd5934e7efc14098048b720720c61e47f021
Alex Chen <axchen7@outlook.com>
2022-10-16 07:50:22 -0400

    Optimize assemblyAi

--- lib/assemblyAi.ts
+++ lib/assemblyAi.ts
@@ -1,4 +1,3 @@
-export const ASSEMBLY_AI_UPLOAD_URL = "https://api.assemblyai.com/v2/upload";
 export const ASSEMBLY_AI_TRANSCRIPT_URL =
   "https://api.assemblyai.com/v2/transcript";
 export const ASSEMBLY_AI_API_KEY = process.env.ASSEMBLY_AI_API_KEY!;
--- pages/api/transcribe/index.ts
+++ pages/api/transcribe/index.ts
@@ -2,7 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import {
   ASSEMBLY_AI_API_KEY,
   ASSEMBLY_AI_TRANSCRIPT_URL,
-  ASSEMBLY_AI_UPLOAD_URL,
 } from "../../../lib/assemblyAi";
 import { VIDEOS_CONTAINER_URL } from "../../../lib/azureStorage";
 import { TranscribeResponse } from "../../../types/frontendModels";
@@ -14,21 +13,6 @@ export default async function handler(
   if (req.method === "GET") {
     const videoName = req.query.videoName as string;
 
-    const videoRes = await fetch(`${VIDEOS_CONTAINER_URL}/${videoName}`, {
-      method: "GET",
-    });
-
-    const uploadRes = await fetch(ASSEMBLY_AI_UPLOAD_URL, {
-      method: "POST",
-      headers: {
-        authorization: ASSEMBLY_AI_API_KEY,
-        "Transfer-Encoding": "chunked",
-      },
-      body: videoRes.body,
-    });
-
-    const { upload_url } = await uploadRes.json();
-
     const transcriptRes = await fetch(ASSEMBLY_AI_TRANSCRIPT_URL, {
       method: "POST",
       headers: {
@@ -36,7 +20,7 @@ export default async function handler(
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        audio_url: upload_url,
+        audio_url: `${VIDEOS_CONTAINER_URL}/${videoName}`,
       }),
     });
 
