a1727c7034a460923102bc6a4710ef8ea634d634
Alex Chen <axchen7@outlook.com>
2022-10-16 07:23:48 -0400

    Move assemblyAI api key to env

--- lib/assemblyAi.ts
+++ lib/assemblyAi.ts
@@ -1,4 +1,4 @@
 export const ASSEMBLY_AI_UPLOAD_URL = "https://api.assemblyai.com/v2/upload";
 export const ASSEMBLY_AI_TRANSCRIPT_URL =
   "https://api.assemblyai.com/v2/transcript";
-export const ASSEMBLY_AI_API_KEY = "e1444bf55fa2465a92bc4f9528976d95";
+export const ASSEMBLY_AI_API_KEY = process.env.ASSEMBLY_AI_API_KEY!;
